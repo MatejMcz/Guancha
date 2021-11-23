8b2e6ac00dfba80582b8fb821144521a67699cc1
matej <matej@notebook>
2021-11-23 19:59:40 +0100

    Added speed and size attributes and a direction map for changing coordinates according to the direction of movement

--- WpfApp1/Entities/Entity.cs
+++ WpfApp1/Entities/Entity.cs
@@ -8,9 +8,22 @@ namespace WpfApp1.Entities
 {
     internal class Entity
     {
+
+
+        public readonly Dictionary<int, int[]> direction_map = new Dictionary<int, int[]>
+        {
+            { 0, new int[] { 0, -1 } },
+            { 1, new int[] { -1, 0 } },
+            { 2, new int[] { 0, 1 } },
+            { 3, new int[] { 1, 0 } }
+        };
+
+
+        
         public int[]  Position { get; set; }
         public int Facing { get; set; }
         public int[] Color { get; set; }
-        
+        public int Speed { get; set; }
+        public int Size { get; set; }
     }
 }
